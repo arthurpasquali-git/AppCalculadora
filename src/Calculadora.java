d85ba8251e010ca126c60162a0b4a0826aaae132
arthurpasquali-git <arthur_pasquali@estudante.sesisenai.org.br>
2025-08-27 16:48:39 -0300

    commit - ajeitando
    Ajeitei a organização do código retirando linhas desnescesárias

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -5,17 +5,13 @@ public class Calculadora {
     public static int subtracao(int a, int b) {
         return a - b;
     }
-
     public static int multiplicacao(int a, int b) {
         return a * b;
-
-        
     }
     public static double divisao(int a, int b) {
         if (b == 0) throw new IllegalArgumentException("Divisão por zero não permitida!");
         return (double) a / b;
-
-}
+    }
 }
 
 
